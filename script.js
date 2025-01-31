412e991561ae0fa34b4e530246362bd05aca912a
Recomagnesius <buzhor.danyl@gmail.com>
2025-01-31 01:13:02 -0600

    add js for toggling text

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const faqAnswers = document.getElementsByClassName('faq__list-item__text')
 const faqAnswersArr = Array.from(faqAnswers);
 const faqIcons = document.getElementsByClassName('faq-toggle');
 const faqIconsArr = Array.from(faqIcons);
-console.log(faqIconsArr);
 faqIconsArr.forEach((e) => {
   e.addEventListener('click', toggleText);
 });
 
-function toggleText(e){
-  e.classList.toggle(hide);
+function toggleText(e) {
+  const parent = e.target.closest('li');
+  console.log(parent)
+  const textForToggle = parent.querySelector('.fac__list-item__text');
+  console.log(textForToggle)
+  textForToggle.classList.toggle('hide');
 }
