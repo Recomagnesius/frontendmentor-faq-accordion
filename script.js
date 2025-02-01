fd05aca368deb7a5b0de24088dae7d74bf44bf73
Recomagnesius <buzhor.danyl@gmail.com>
2025-02-01 15:52:52 -0600

    finish styling and js

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-const faqAnswers = document.getElementsByClassName('faq__list-item__text')
-const faqAnswersArr = Array.from(faqAnswers);
-const faqIcons = document.getElementsByClassName('faq-toggle');
-const faqIconsArr = Array.from(faqIcons);
-faqIconsArr.forEach((e) => {
-  e.addEventListener('click', toggleText);
+const faqButtons = document.getElementsByClassName('faq-toggle');
+const faqButtonsArr = Array.from(faqButtons);
+const plusPath = './assets/images/icon-plus.svg';
+const minusPath = './assets/images/icon-minus.svg';
+
+faqButtonsArr.forEach((e) => {
+  e.addEventListener('click', ToggleTextAndSign);
 });
+
+function ToggleTextAndSign(e) { 
+  
+  toggleSign(e);
+  toggleText(e);
+}
 
 function toggleText(e) {
   const parent = e.target.closest('li');
-  console.log(parent)
-  const textForToggle = parent.querySelector('.fac__list-item__text');
-  console.log(textForToggle)
+  const textForToggle = parent.querySelector('.faq__list-item__text');
+  
   textForToggle.classList.toggle('hide');
+  textForToggle.classList.toggle('show');
+}
+
+function toggleSign(e) { 
+  const parent = e.target.closest('li');
+  console.log(parent)
+  const img = parent.querySelector('.faq-icon');
+  console.log(img)
+  if (img.src.includes('icon-plus.svg')) {
+    img.src = minusPath;
+  }
+  else { 
+    img.src = plusPath;
+  }
+
 }
